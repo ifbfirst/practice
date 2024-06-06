01b2d4c78b0c72c35633050ddf5bb42fd5263a18
Kate Mashko <kat_zb@mail.ru>
2024-06-06 13:44:04 +0300

    fix: best solution

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const output = document.createElement("div");
 document.body.append(input, btn, output);
 
 btn.addEventListener("click", function changeDigitalIntoBinary() {
-  if (!Number(input.value)) alert("Введите число");
-  let str = "";
-  let value = input.value;
-
-  while (value > 0) {
-    let binary;
-    binary = value % 2;
-    value = parseInt(value / 2);
-    str = `${binary}${str}`;
+  if (isNaN(+input.value)) {
+    alert("Введите число");
+    return;
   }
+  output.textContent = (+input.value).toString(2);
+  // let str = "";
+  // let value = input.value;
 
-  output.textContent = str;
+  // while (value > 0) {
+  //   let binary;
+  //   binary = value % 2;
+  //   value = parseInt(value / 2);
+  //   str = `${binary}${str}`;
+  // }
+
+  // output.textContent = str;
 });
