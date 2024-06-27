7edf1843340fa666eaa1382e08499d2b5d76d403
Kate Mashko <kat_zb@mail.ru>
2024-06-27 20:11:44 +0300

    feat: add solution for 3-2 lesson

--- lecture3-2.js
+++ lecture3-2.js
@@ -1,16 +1,47 @@
-let arr = [];
-const obj = {
-  userName,
-  userAge,
+let users = [];
+
+const user = {
+  userName: null,
+  userAge: null,
 };
 
 function addPerson(name, age) {
-  obj.__proto__ = arr.push({
-    name,
-    age,
-  });
-  console.log(arr);
+  const random = Math.random();
+  let newUser;
+  switch (true) {
+    //1.literal
+    case random <= 0.25:
+      newUser = {
+        name,
+        age,
+      };
+      break;
+    case 0.25 < random && 0.5 >= random:
+      //2.Object.create
+      newUser = Object.create(user);
+      newUser.userName = name;
+      newUser.userAge = age;
+      break;
+    case 0.5 < random && 0.75 >= random:
+      // 3. new Object
+      newUser = new Object();
+      newUser.userName = name;
+      newUser.userAge = age;
+      break;
+    case 0.75 < random:
+      // 4. Object.assign
+      newUser = Object.assign({}, { userName: name, userAge: age });
+      break;
+    default:
+      newUser = Object.assign({}, { userName: name, userAge: age });
+  }
+
+  users.push(newUser);
+
+  return users;
 }
 addPerson("Коля", 17);
 addPerson("Маша", 22);
 addPerson("Паша", 19);
+addPerson("Саша", 24);
+addPerson("Лена", 18);
